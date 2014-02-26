269dcca49a8a448a2d00aad33143a8ce099e4c66
Root VirtualBox PUM <programelo.usted.mismo@gmail.com>
2014-02-25 23:22:05 -0500

    Commit

--- populate.py
+++ populate.py
@@ -1,179 +1,193 @@
+import MySQLdb as mdb
 from csv import *
 from sys import argv,exit
 
-tabledir="tmp/EspaciosFacultad/"
-basename="Salomon-BasesDatos"
+tabledir="tmp/EspaciosFacultad/BaseDatos/"
+basename="Salomon"
 
-#USE
-print """use salomon_1401;
-truncate table Dependencias;
-truncate table Programas;
-truncate table Espacios;
-truncate table Recursos;
-truncate table Actividades;
-truncate table Horarios;
-"""
+con=mdb.connect("localhost","salomon","123","salomon_1401")
+db=con.cursor()
 
-"""
-#SIMPLE
-tablas=["Programas","Dependencias"]
-for tabla in tablas:
+verbose=False
+#verbose=True
+
+with con:
+
+    ##################################################
+    #RESET
+    ##################################################
+    for table in ["Dependencias","Programas","Espacios",
+                  "Recursos","Actividades","Horarios"]:
+        print "Truncando tabla %s..."%table
+        sql="truncate table %s;"%table
+        if verbose:print sql
+        db.execute(sql)
+
+    ##################################################
+    #PROGRAMAS Y DEPENDENCIAS
+    ##################################################
+    tablas=["Programas","Dependencias"]
+    for tabla in tablas:
+        print "Guardando tabla %s..."%tabla
+        fl=open(tabledir+basename+"-%s.csv"%(tabla),"rU")
+        data=reader(fl,delimiter=";")
+        i=0
+        for row in data:
+            if i==0:
+                fields=",".join(row)
+                campos="(%s)"%fields
+            else:
+                valores="('"+"','".join(row)+"')"
+                sql="insert into %s %s values %s;"%(tabla,campos,valores)
+                if verbose:print sql
+                db.execute(sql)
+            i+=1
+        fl.close()
+
+    ##################################################
+    #ESPACIOS
+    ##################################################
+    print "Guardando tabla de Espacios..."
+    tabla="Espacios"
     fl=open(tabledir+basename+"-%s.csv"%(tabla),"rU")
     data=reader(fl,delimiter=";")
+    
     i=0
+    jr=1000
     for row in data:
         if i==0:
-            fields=",".join(row)
-            campos="(%s)"%fields
+            fields=""
+            fields_rec=""
+            j=0
+            for field in row:
+                if field=="recurso_id":
+                    jr=j
+                if j>jr:
+                    fields_rec+="%s,"%field
+                else:
+                    fields+="%s,"%field
+                j+=1
+            campos="(%s)"%(fields.strip(","))
+            campos_rec="(%srecurso)"%(fields_rec)
         else:
-            valores="('"+"','".join(row)+"')"
-            sql="insert into %s %s values %s;"%(tabla,campos,valores)
-            print sql
+            values=""
+            values_rec=""
+            j=0
+            for value in row:
+                if j==jr:recursoid=value
+                if j>jr:
+                    values_rec+="'%s',"%value
+                else:
+                    values+="'%s',"%value
+                j+=1
+            values=values.strip(",")
+            values_rec=values_rec+"'%s'"%(recursoid)
+
+            sql="insert into %s %s values (%s);"%(tabla,campos,values)
+            if verbose:print sql
+            db.execute(sql)
+            sql="insert into %s %s values (%s);"%("Recursos",campos_rec,values_rec)
+            if verbose:print sql
+            db.execute(sql)
         i+=1
     fl.close()
 
-#ESPACIOS
-tabla="Espacios"
-fl=open(tabledir+basename+"-%s.csv"%(tabla),"rU")
-data=reader(fl,delimiter=";")
+    ##################################################
+    #ACTIVIDADES
+    ##################################################
+    for programa in ['Fisica','Biologia','Quimica','Matematicas','Facultad']:
+    #for programa in ['fisica']:
+        print "Guardando las actividades de %s..."%programa
+        fl=open(tabledir+basename+"-Actividades-%s.csv"%programa,"rU")
+        data=reader(fl,delimiter=";")
 
-i=0
-jr=1000
-for row in data:
-    if i==0:
-        fields=""
-        fields_rec=""
-        j=0
-        for field in row:
-            if field=="recurso_id":
-                jr=j
-            if j>jr:
-                fields_rec+="%s,"%field
+        i=0
+        jh=10000
+        jr=10000
+        cursos=dict()
+        first=True
+        
+        n=0
+        for row in data:
+            if i==0:
+                fields=""
+                fields_hor=""
+                fields_rec=""
+                j=0
+                kh=0
+                kr=0
+                for field in row:
+                    if field=="horario_id":
+                        hid=j
+                    if field=="recurso_id":
+                        rid=j
+                    if field=="dia":
+                        jh=j
+                    if field=="capacidad":
+                        jr=j
+                    if j>=jh and kh<3:
+                        fields_hor+="%s,"%field
+                        kh+=1
+                    if j>=jr and kr<100:
+                        fields_rec+="%s,"%field
+                        kr+=1
+                    if kh<=3 and kr<=3 and j<jh:
+                        if field!='horario_id' and field!="espacio_id" and field!="recurso_id":
+                            fields+="%s,"%field
+                    j+=1
+                campos="(recurso_id,%s)"%(fields.strip(","))
+                campos_hor="(horario,codigo_id,%s)"%(fields_hor.strip(","))
+                campos_rec="(recurso,%s)"%(fields_rec.strip(","))
             else:
-                fields+="%s,"%field
-            j+=1
-        campos="(%s)"%(fields.strip(","))
-        campos_rec="(%srecurso)"%(fields_rec)
-    else:
-        values=""
-        values_rec=""
-        j=0
-        for value in row:
-            if j==jr:recursoid=value
-            if j>jr:
-                values_rec+="'%s',"%value
-            else:
-                values+="'%s',"%value
-            j+=1
-        values=values.strip(",")
-        values_rec=values_rec+"'%s'"%(recursoid)
+                values=""
+                values_hor=""
+                values_rec=""
+                j=0
+                horarioid=""
+                recursoid=""
+                for value in row:
+                    if j==0:
+                        codigo=value
+                        if value not in cursos.keys():
+                            n+=1
+                            cursos[value]=""
+                            qinsert=True
+                        else:
+                            qinsert=False
+                    if j<jh-3:
+                        values+="'%s',"%value.strip()
+                    elif j>=jh and j<jr and 'id' not in value:
+                        values_hor+="'%s',"%value.strip()
+                    elif j>=jr and 'id' not in value:
+                        values_rec+="'%s',"%value.strip()
+                    else:
+                        pass
+                    if j==hid:
+                        horarioid=value
+                        cursos[codigo]+="%s;"%horarioid
+                    if j==rid:
+                        recursoid=value
+                    j+=1
 
-        sql="insert into %s %s values (%s);"%(tabla,campos,values)
-        print sql
-        sql="insert into %s %s values (%s);"%("Recursos",campos_rec,values_rec)
-        print sql
+                values="'A%s'"%codigo+","+values.strip(",")
+                values_hor="'%s','%s',"%(horarioid,codigo)+values_hor.strip(",")
+                values_rec="'%s',"%recursoid+values_rec.strip(",")
 
-    i+=1
+                if qinsert:
+                    sql="insert into Actividades %s values (%s);"%(campos,values)
+                    if verbose:print sql
+                    db.execute(sql)
+            
+                sql="insert into Horarios %s values (%s);"%(campos_hor,values_hor)
+                if verbose:print sql
+                db.execute(sql)
+                sql="insert into Recursos %s values (%s);"%(campos_rec,values_rec)
+                if verbose:print sql
+                db.execute(sql)
+            i+=1
+            #if i>5:break
 
-fl.close()
-#"""
-
-#"""
-#ACTIVIDADES
-tabla="Actividades"
-#fl=open(tabledir+basename+"-%s.csv"%(tabla),"rU")
-fl=open(tabledir+"actividades-fisica.csv","rU")
-
-data=reader(fl,delimiter=";")
-
-i=0
-jh=10000
-jr=10000
-cursos=dict()
-first=True
-n=0
-for row in data:
-    if i==0:
-        fields=""
-        fields_hor=""
-        fields_rec=""
-        j=0
-        kh=0
-        kr=0
-        for field in row:
-            if field=="horario_id":
-                hid=j
-            if field=="recurso_id":
-                rid=j
-            if field=="dia":
-                jh=j
-            if field=="capacidad":
-                jr=j
-            if j>=jh and kh<3:
-                fields_hor+="%s,"%field
-                kh+=1
-            if j>=jr and kr<100:
-                fields_rec+="%s,"%field
-                kr+=1
-            if kh<=3 and kr<=3 and j<jh:
-                if field!='horario_id' and field!="espacio_id" and field!="recurso_id":
-                    fields+="%s,"%field
-            j+=1
-        campos="(recurso_id,%s)"%(fields.strip(","))
-        campos_hor="(horario,%s)"%(fields_hor.strip(","))
-        campos_rec="(recurso,%s)"%(fields_rec.strip(","))
-    else:
-        values=""
-        values_hor=""
-        values_rec=""
-        j=0
-        horarioid=""
-        recursoid=""
-        for value in row:
-            if j==0:
-                codigo=value
-                if value not in cursos.keys():
-                    n+=1
-                    cursos[value]=""
-                    qinsert=True
-                    """
-                    sql="insert into %s (codigo) values ('%s');"%(tabla,value)
-                    print sql
-                    """
-                else:
-                    qinsert=False
-            if j<jh-3:
-                values+="'%s',"%value.strip()
-            elif j>=jh and j<jr and 'id' not in value:
-                values_hor+="'%s',"%value.strip()
-            elif j>=jr and 'id' not in value:
-                values_rec+="'%s',"%value.strip()
-            else:
-                pass
-            if j==hid:
-                horarioid=value
-                cursos[codigo]+="%s;"%horarioid
-            if j==rid:
-                recursoid=value
-            j+=1
-        values="'A%s'"%codigo+","+values.strip(",")
-        values_hor="'"+horarioid+"',"+values_hor.strip(",")
-        values_rec="'"+recursoid+"',"+values_rec.strip(",")
-
-        #sql="delete from %s where codigo='%s';\n"%(tabla,codigo)
-        if qinsert:
-            sql="select 'Inserting course %d %s';\n"%(n,codigo)
-            sql+="insert into %s %s values (%s);"%(tabla,campos,values)
-            print sql
-        sql="insert into Horarios %s values (%s);"%(campos_hor,values_hor)
-        print sql
-        sql="insert into Recursos %s values (%s);"%(campos_rec,values_rec)
-        print sql
-    i+=1
-    #if i>5:break
-
-for curso in cursos.keys():
-    sql="update Actividades set horario_ids='%s' where codigo='%s';"%(cursos[curso],curso)
-    print sql
-
-#"""
+        for curso in cursos.keys():
+            sql="update Actividades set horario_ids='%s' where codigo='%s';"%(cursos[curso],curso)
+            if verbose:print sql
+            db.execute(sql)
--- tables.sql
+++ tables.sql
@@ -3,7 +3,7 @@ drop table if exists Recursos,Actividades,Espacios,Horarios,Dependencias,Program
 
 create table Dependencias (
     #PROPIEDADES
-    dependencia tinyint,
+    dependencia varchar(2),
     primary key (dependencia),
 
     nombre varchar(255),
@@ -15,76 +15,73 @@ create table Dependencias (
 
 create table Programas (
     #PROPIEDADES
-    programa varchar(20) not null,
+    programa varchar(3) not null,
     primary key (programa),
 
     nombre varchar(255),
 
     #ENLACES
-    dependencia_id tinyint
+    dependencia_id varchar(2)
 );
 
 create table Espacios (
-
     #PROPIEDADES
     espacio varchar(5),
     primary key (espacio),
     
-    bloque smallint,
-    numero mediumint,
+    bloque varchar(2),
+    numero varchar(3),
 
     #ENLACES
-    dependencia_id smallint,
-    recurso_id smallint,
-    horario_ids varchar(255)
+    dependencia_id varchar(2),
+    recurso_id varchar(50),
+    horario_ids varchar(1000)
 );
 
 create table Recursos (
 
     #PROPIEDADES
-    recurso varchar(20),
+    recurso varchar(50),
     primary key (recurso),
     
-    capacidad smallint,
-    salacomputo tinyint(1) not null default 0,
-    labquim tinyint(1) not null default 0,
-    proyfijo tinyint(1) not null default 0,
-    proymovil tinyint(1) not null default 0,
-    tv tinyint(1) not null default 0,
-    pantalla tinyint(1) not null default 0,
-    computador tinyint(1) not null default 0,
-    laptop tinyint(1) not null default 0,
-    extension tinyint(1) not null default 0,
-    red tinyint(1) not null default 0,
-    tabacrilico tinyint(1) not null default 0,
-    tabtiza tinyint(1) not null default 0,
-    tabvidrio tinyint(1) not null default 0,
-    mesa tinyint(1) not null default 0,
-    atril tinyint(1) not null default 0,
-    torremul tinyint(1) not null default 0,
-    parlantes tinyint(1) not null default 0,
-    camara tinyint(1) not null default 0,
-    aire tinyint(1) not null default 0,
-    ventilador tinyint(1) not null default 0,
-    sillas_moviles tinyint(1) not null default 0,
-    opacas tinyint(1) not null default 0,
-    cortinas tinyint(1) not null default 0,
-    wifi tinyint(1) not null default 0,
-    windows tinyint(1) not null default 0,
-    linux tinyint(1) not null default 0
-
+    capacidad varchar(3),
+    salacomputo varchar(1) not null default 0,
+    labquim varchar(1) not null default 0,
+    proyfijo varchar(1) not null default 0,
+    proymovil varchar(1) not null default 0,
+    tv varchar(1) not null default 0,
+    pantalla varchar(1) not null default 0,
+    computador varchar(1) not null default 0,
+    laptop varchar(1) not null default 0,
+    extension varchar(1) not null default 0,
+    red varchar(1) not null default 0,
+    tabacrilico varchar(1) not null default 0,
+    tabtiza varchar(1) not null default 0,
+    tabvidrio varchar(1) not null default 0,
+    mesa varchar(1) not null default 0,
+    atril varchar(1) not null default 0,
+    torremul varchar(1) not null default 0,
+    parlantes varchar(1) not null default 0,
+    camara varchar(1) not null default 0,
+    aire varchar(1) not null default 0,
+    ventilador varchar(1) not null default 0,
+    sillas_moviles varchar(1) not null default 0,
+    opacas varchar(1) not null default 0,
+    cortinas varchar(1) not null default 0,
+    wifi varchar(1) not null default 0,
+    windows varchar(1) not null default 0,
+    linux varchar(1) not null default 0
 );
 
 create table Horarios (
-
     #PROPIEDADES
     horario varchar(20),
     primary key (horario),
     
-    dia char,
-    hora smallint,
-    duracion smallint,
-    eficiencia smallint,
+    dia varchar(2),
+    hora varchar(2),
+    duracion varchar(2),
+    eficiencia varchar(20),
 
     #ENLACES
     codigo_id varchar(20),
@@ -97,12 +94,11 @@ create table Actividades (
     primary key (codigo),
 
     nombre varchar(255),
-    grupo tinyint,
-    matriculados mediumint,
+    grupo varchar(2),
+    matriculados varchar(3),
 
     #ENLACES
-    recurso_id mediumint,
-    programa_id varchar(5),
-    horario_ids varchar(255)
+    recurso_id varchar(50),
+    programa_id varchar(3),
+    horario_ids varchar(1000)
 );
-
