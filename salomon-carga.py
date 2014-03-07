2e63d79d06e5491efd88036839d36a7b690a307f
Root VirtualBox PUM <programelo.usted.mismo@gmail.com>
2014-03-07 04:47:19 -0500

    Commit

---
+++ salomon-carga.py
@@ -0,0 +1,23 @@
+from util import *
+
+salomon_file=argv[1]
+
+#############################################################
+#LOAD DATABASE
+#############################################################
+salomon,connection=loadDatabase(server='localhost',
+                                user='salomon',
+                                password='123',
+                                database='salomon_1401')
+
+#############################################################
+#LOAD DATA
+#############################################################
+print "Unpackaging Salomon database in file '%s'..."%salomon_file
+salomon_load=unpickleSalomon(salomon_file)
+
+#############################################################
+#SAVE DATABASE
+#############################################################
+print "Updating database..."
+updateDatabase(salomon_load,connection)
